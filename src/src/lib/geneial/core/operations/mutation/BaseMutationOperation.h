8daaf0b93df4f311844cc167414347eaf3955545
Netz <lukas.netz@rwth-aachen.de>
2014-12-28 12:54:01 +0100

    Basic Mutation Settings and Classes.

---
+++ src/src/lib/geneial/core/operations/mutation/BaseMutationOperation.h
@@ -0,0 +1,46 @@
+/*
+ * BaseMutationOperation.h
+ *
+ *  Created on: Dec 19, 2014
+ *      Author: lukas
+ */
+
+#include <geneial/core/operations/mutation/MutationSettings.h>
+#include <geneial/core/population/management/BaseManager.h>
+
+using namespace GeneticLibrary::Population::Manager;
+
+namespace GeneticLibrary {
+namespace Opertation {
+namespace Mutation {
+
+template <typename FITNESS_TYPE>
+class BaseMutationOperation {
+private:
+	MutationSettings* _settings;
+public:
+	typedef typename Population::Population<FITNESS_TYPE>::chromosome_container mutation_result_set;
+	BaseMutationOperation(MutationSettings *settings): _settings(settings) {}; //Constructor
+	virtual ~BaseMutationOperation() {}; //Destructor
+
+	virtual mutation_result_set doMutate
+				(
+						typename BaseMutationOperation<FITNESS_TYPE>::mutation_result_set,
+						BaseMutationOperation<FITNESS_TYPE> *mutationOperation,
+						BaseManager<FITNESS_TYPE> &manager
+				) = 0; //Wofür das bei doCopulate ??
+
+	MutationSettings* const& getSettings() const {
+		return _settings;
+	};
+
+	void setSettings(const MutationSettings*& settings) {
+		_settings = settings;
+	};
+};
+
+} //namespace Mutation
+} //namespace Operation
+} //namespace GeneticLibrary
+
+
---
+++ src/src/lib/geneial/core/operations/mutation/MutationSettings.h
@@ -0,0 +1,63 @@
+/*
+ * MutationSettings.h
+ *
+ *  Created on: Dec 28, 2014
+ *      Author: lukas
+ */
+
+#ifndef SRC_LIB_GENEIAL_CORE_OPERATIONS_MUTATION_MUTATIONSETTINGS_H_
+#define SRC_LIB_GENEIAL_CORE_OPERATIONS_MUTATION_MUTATIONSETTINGS_H_
+
+#include "assert.h"
+namespace GeneticLibrary {
+namespace Operation {
+namespace Mutation {
+
+class MutationSettings{
+private:
+	double _propabilityOfMutation; //propability of the mutation for one Chromosome
+	double _levelOfMutation; //amount of change caused to one chromosome
+	double _amountOfMutation; //points of change in one chromosome
+
+public:
+	virtual ~MutationSettings(){};
+
+	MutationSettings(double propabilityOfMutation, double levelOfMutation, double amountOfMutation){
+		_propabilityOfMutation = propabilityOfMutation;
+		_levelOfMutation = levelOfMutation;
+		_amountOfMutation = amountOfMutation;
+	};
+
+	double getPropabilityOfMutation() const {
+		return _propabilityOfMutation;
+	};
+
+	double getLevelOfMutation() const {
+		return _levelOfMutation;
+	};
+
+	double getAmountOfMutation() const {
+		return _amountOfMutation;
+	};
+
+	void setPropabilityOfMutation(double propabilityOfMutation){
+		_propabilityOfMutation = propabilityOfMutation;
+	};
+
+	void setLevelOfMutation(double levelOfMutation){
+		_levelOfMutation = levelOfMutation;
+	};
+
+	void setAmountOfMutation(double amountOfMutation){
+		_amountOfMutation = amountOfMutation;
+	};
+};//Class MutationSettings
+
+
+}//namespace Mutation
+}//namespace Operation
+}//namespace Genetic Library
+
+
+
+#endif /* SRC_LIB_GENEIAL_CORE_OPERATIONS_MUTATION_MUTATIONSETTINGS_H_ */
