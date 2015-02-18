5f28ce752ff6cfc86ba7e4043d199457fa57108f
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2015-02-18 19:25:00 +0100

    stopping criterion requires a print function

--- src/src/lib/geneial/algorithm/criteria/BaseStoppingCriterion.h
+++ src/src/lib/geneial/algorithm/criteria/BaseStoppingCriterion.h
@@ -17,9 +17,18 @@ namespace Algorithm {
 namespace StoppingCriteria {
 
 template <typename FITNESS_TYPE>
-class BaseStoppingCriterion{
+class BaseStoppingCriterion: public Utility::printable,
+							 public boost::enable_shared_from_this<BaseChromosome <FITNESS_TYPE> >
+{
 public:
+
+	typedef typename boost::shared_ptr<BaseStoppingCriterion <FITNESS_TYPE> > ptr;
+	typedef typename boost::shared_ptr<const BaseStoppingCriterion <FITNESS_TYPE> > const_ptr;
+
 	virtual ~BaseStoppingCriterion(){};
+
+	virtual void print(std::ostream& os) const = 0;
+	
 	virtual bool wasReached(BaseManager<FITNESS_TYPE> &manager) = 0;
 };
 
