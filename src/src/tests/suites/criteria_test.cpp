ca5a8b26d6f9d5a679088ac16d034bf984417a2a
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2015-02-19 20:01:06 +0100

    criteria test stub

---
+++ src/src/tests/suites/criteria_test.cpp
@@ -0,0 +1,54 @@
+#define BOOST_TEST_DYN_LINK
+
+#define BOOST_TEST_MODULE algorithm/criteria
+
+#include <boost/test/unit_test.hpp>
+
+#include <geneial/algorithm/criteria/CombinedCriterion.h>
+
+#include <geneial/algorithm/criteria/NegationDecorator.h>
+#include <geneial/algorithm/criteria/ConsecutiveDecorator.h>
+
+#include <geneial/algorithm/criteria/FitnessValueDeltaCriterion.h>
+#include <geneial/algorithm/criteria/FitnessValueReachedCriterion.h>
+#include <geneial/algorithm/criteria/PopulationUnchangedCriterion.h>
+#include <geneial/algorithm/criteria/TrailingStopCriterion.h>
+#include <geneial/algorithm/criteria/FixPointCriterion.h>
+
+
+
+BOOST_AUTO_TEST_SUITE( CombinedCriterion )
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( NegationDecorator )
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( ConsecutiveDecorator )
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
+
+
+BOOST_AUTO_TEST_SUITE( FitnessValueDeltaCriterion )
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( FitnessValueReachedCriterion )
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( PopulationUnchangedCriterion )
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( TrailingStopCriterion )
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( FixPointCriterion )
+BOOST_AUTO_TEST_SUITE_END()
+
+
