5b74801e69062d65c40e9498605f79901725739d
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2015-02-21 17:22:37 +0100

    updated autobuilder to have more options

--- autobuild.sh
+++ autobuild.sh
@@ -1,30 +1,93 @@
 #!/bin/bash
+#@description simple guided noob frontend for building geneial
 #@author Benedikt Wolters<benedikt.wolters@rwth-aachen.de>
+
+
+BUILDDIR=build/
+
 
 #show all commands from here
 set -x
-
+function cleanup_and_exit {
+   rm -rf $BUILDDIR
+   exit
+}
 #stop on any nonzero error
 set -e 
-mkdir -p build/
-cd build/
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -D CMAKE_BUILD_TYPE=Debug ../src
-make
-make test
+mkdir -p $BUILDDIR
+cd $BUILDDIR
 
-set +x
+REPLY=
+until [[ $REPLY = [RrDd] ]]; do
+    read -p "Build [R]elease version or [D]ebug version?  [r/d]:" -n 1 -r
+    echo    # (optional) move to a new line
+done
 
-echo "Build complete."
+
+BUILD_TYPE="Debug"
+if [[ $REPLY = [Rr] ]]
+then
+    BUILD_TYPE="Release"
+fi
+
 
 #as long as we read invalid stuff prompt the user
 REPLY=
-until [[ $REPLY =~ ^[YyNn]$ ]]; do
+until [[ $REPLY = [YyNn] ]]; do 
+    read -p "Want to generate an Eclipse CDT4 Project? [y/n]" -n 1 -r
+    echo    # (optional) move to a new line
+done
+
+projargs=()
+if [[ $REPLY = [Yy] ]]; then
+    projargs=( -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE )
+fi
+
+cmake "${projargs[@]}" -D CMAKE_BUILD_TYPE=$BUILD_TYPE ../src
+
+make VERBOSE=1
+
+
+#as long as we read invalid stuff prompt the user
+REPLY=
+until [[ $REPLY = [YyNn] ]]; do
+    read -p "Run unit test now? [y/n]" -n 1 -r
+    echo    # (optional) move to a new line
+done
+
+#make install upon yes
+if [[ $REPLY = [Yy] ]]
+then
+    if testout=$(make test); then
+        echo "Test succeeded!"
+    else
+        REPLY=
+        until [[ $REPLY = [YyNn] ]]; do
+            read -p "Some test failed, continue? [y/n]:" -n 1 -r
+            echo    # (optional) move to a new line
+        done
+
+        #make install upon yes
+        if [[ $REPLY = [Nn] ]]
+        then
+            cleanup_and_exit
+        fi
+    fi
+fi
+
+echo "Build complete."
+
+set +x
+
+#as long as we read invalid stuff prompt the user
+REPLY=
+until [[ $REPLY = [YyNn] ]]; do
     read -p "Also wish to install?  [y/n]:" -n 1 -r
     echo    # (optional) move to a new line
 done
 
 #make install upon yes
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY = [Yy] ]]
 then
     sudo make install
 fi
