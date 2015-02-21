7252ae3d5f62efb1f7e7a65233847827fc696b4c
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2015-02-21 15:44:02 +0100

    bugfixes in random

--- src/src/lib/geneial/utility/Random.cpp
+++ src/src/lib/geneial/utility/Random.cpp
@@ -3,25 +3,35 @@
 #include <time.h>       /* time */
 #include <cstdlib> 		/* RAND_MAX */
 #include <limits>       /* min, max of float, double */
+#include <iostream>
 
 #include <geneial/utility/Random.h>
-#include "assert.h"
+#include <cassert>
 
 namespace GeneticLibrary {
 namespace Utility {
 
-Random::Random() {
-	//Initialize randomness
+//Assures we have positive mod every time. (because in cpp -6 % 5 = -1)
+inline int mymod(int k, int n)
+{
+    return ((k %= n) < 0) ? k+n : k;
+}
+
+
+Random::Random()
+{
 	srand (static_cast <unsigned> (time(0)));
 }
 
 
-bool Random::generateBit() const {
+bool Random::generateBit() const
+{
 	return rand()&1;
 }
 
 
-bool Random::decision(const double probability) const{
+bool Random::decision(const double probability) const
+{
 	assert(0.0 <= probability);
 	assert(1.0 >= probability);
 	const double reference = generateDouble(0.0,1.0);
@@ -33,35 +43,54 @@ bool Random::decision(const double probability) const{
 	}
 }
 
-int Random::generateInt() const{
+int Random::generateInt() const
+{
 	return rand();
 }
 
-int Random::generateInt(const int min, const int max)const{
-	 return rand() % (max - min + 1) + min;
+int Random::generateInt(const int min, const int max) const
+{
+	assert(min <= max);
+	int sign = 1;
+	if(min < 0)
+	{
+		sign *= -(rand()  % 2);
+	}
+	const int result = mymod(sign*rand(), (max - min + 1)) + min;
+	return result;
 }
 
-double Random::generateDouble(const double min, const double max)const{
-    double range = (max - min);
-    double div = RAND_MAX / range;
-    double random = min + (rand() / div);
+double Random::generateDouble(const double min, const double max) const
+{
+	assert(min <= max);
+	int sign = 1;
+	if(min < 0){
+		sign *= -(rand()  % 2);
+	}
+    const double range = (max - min);
+    const double div = RAND_MAX / range;
+    const double random = min + (rand() / div);
     return random;
 }
 
-double Random::generateDouble() const{
+double Random::generateDouble() const
+{
 	return generateDouble(
 			std::numeric_limits<double>::min(),
 			std::numeric_limits<double>::max());
 }
 
-float Random::generateFloat(const float min, const float max) const{
-	float range = (max - min);
-	float div = ((float) RAND_MAX) / range;
-	float random = min + (((float) rand()) / div);
+float Random::generateFloat(const float min, const float max) const
+{
+	assert(min <= max);
+	const float range = (max - min);
+	const float div = ((float) RAND_MAX) / range;
+	const float random = min + (((float) rand()) / div);
     return random;
 }
 
-float Random::generateFloat() const{
+float Random::generateFloat() const
+{
 	return generateFloat(
 			std::numeric_limits<float>::min(),
 			std::numeric_limits<float>::max());
