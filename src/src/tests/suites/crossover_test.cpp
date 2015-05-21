e3ee5314e8ec5b10e6c4f05a0fb0213d04426f94
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2015-05-21 23:45:06 +0200

    test stub for crossover unit test to remind me i need to write this

---
+++ src/src/tests/suites/crossover_test.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_DYN_LINK
+
+#define BOOST_TEST_MODULE algorithm/crossover
+
+#include <boost/test/unit_test.hpp>
+
+//TODO(bewo): Write this tests..
+
+BOOST_AUTO_TEST_SUITE( __CRITERIA_TEST_CombinedCriterion )
+BOOST_AUTO_TEST_CASE( Continuity_Factory )
+{
+    BOOST_CHECK(1==1); //alibi
+}
+BOOST_AUTO_TEST_SUITE_END()
