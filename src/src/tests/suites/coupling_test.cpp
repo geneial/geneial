aab31c2f89de4b1bddd65e30a5ff1369313e781f
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2015-06-06 18:42:18 +0200

    test stubs

---
+++ src/src/tests/suites/coupling_test.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_DYN_LINK
+
+#define BOOST_TEST_MODULE algorithm/crossover
+
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE( __COUPLING_TEST_LambdaAdapterTest )
+BOOST_AUTO_TEST_CASE( LambdaAdapterTest )
+{
+    BOOST_CHECK(1==1);
+}
+BOOST_AUTO_TEST_SUITE_END()
