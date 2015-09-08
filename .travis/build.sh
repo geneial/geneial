2beb767ac047acc8257210f9f9918c588ffc90c8
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2015-09-08 18:10:42 +0200

    travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,5 +3,21 @@
 mkdir build || return 1
 cd build/ || return 1
 cmake ../src || return 1
-make
+
+if [[ ${TASK} == "doxygen" ]]; then
+	make doc
+	
+	git config --global user.email "benedikt.wolters@rwth-aachen.de"
+	git config --global user.name "Travis Doxygen Builder"
+	git clone https://${SECRET_ACCESS_TOKEN}@github.com/geneial/geneial.github.io.git
+	cd geneial.github.io/ || return 1
+	
+	cp ../doc/html/* ./ || return 1
+	git add . || return 1
+	git commit -m "Updated documentation for geneial" || return 1
+	git push origin master || return 1
+
+else
+	make -j1 || return 1
+fi
 
