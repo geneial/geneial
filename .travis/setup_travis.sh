f18b04d77ced3d367ee6e8544c78f66d16df8a5f
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2016-06-16 14:02:12 +0200

    fix travis

---
+++ .travis/setup_travis.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function defCXX {
+	export CC="$1"
+	export CXX="$2"
+}
+
+
+
+
+cmake --version
+
+if [[ ${USE} == "g++-4.8" ]]; then
+	defCXX gcc-4.8 g++-4.8
+elif [[ ${USE} == "g++-4.9" ]]; then
+	defCXX gcc-4.9 g++-4.9
+elif [[ ${USE} == "g++-5" ]]; then
+	defCXX gcc-5 g++-5
+elif [[ ${USE} == "clang++-3.4" ]]; then
+	defCXX clang clang++
+	export CXXFLAGS="-stdlib=libc++"
+elif [[ ${USE} == "clang++-3.5" ]]; then
+	defCXX clang-3.5 clang++-3.5
+	export CXXFLAGS="-stdlib=libc++"
+elif [[ ${USE} == "clang++-3.6" ]]; then
+	defCXX clang-3.6 clang++-3.6
+elif [[ ${USE} == "clang++-3.7" ]]; then
+	defCXX clang-3.7 clang++-3.7
+elif [[ ${USE} == "clang++-3.8" ]]; then
+	defCXX clang-3.8 clang++-3.8
+fi
